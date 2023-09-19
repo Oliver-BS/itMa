52192ba2add1a7022c3c59bdc0facb79728d19e8
Snowiiiiiii <145099815+Snowiiiiiii@users.noreply.github.com>
2023-09-19 12:50:16 +0200

    Update oppgave5kapiBANDERS.py
    Fra Celsius til Fahrenheit

--- oppgave5kapiBANDERS.py
+++ oppgave5kapiBANDERS.py
@@ -1,1 +1,8 @@
+#Vi skal endre tempraturverdiene fra Celcius til Fahrenheit.
 
+Celsius = input("Hvor mange grader celcius er det?")
+Celsius = int(Celsius)
+
+Fahrenheit = (Celsius × 9/5) + 32
+
+print("Det er", Fahrenheit, "grader Fahrenheit ute")
